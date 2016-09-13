7d618c2aec47eb785a2a50dd304dea5906c5e794
Corey Mosher <corey.mosher@gmail.com>
2016-09-13 20:27:04 -0300

    Moving the UI to a separate project

--- ui/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('ui', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr']);
-
-})();
